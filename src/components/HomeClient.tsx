cbd581ee948f541d0c1fc0668d64489205ed7363
LamPham90 <phuonglampp4@gmail.com>
2026-08-10 09:57:42 +0700

    \\

--- src/components/HomeClient.tsx
+++ src/components/HomeClient.tsx
@@ -801,7 +801,7 @@ export default function HomeClient({ initialSections, initialHeroMovies, allCate
 
                   {/* Gradient Overlay Tối Ưu Tương Phản Nhìn Rõ Nội Dung */}
                   <div className="absolute inset-0 bg-gradient-to-r from-black/90 via-black/30 to-transparent z-10 hidden md:block" />
-                  <div className="absolute inset-0 bg-gradient-to-t from-black via-black/70 to-black/30 z-10 md:hidden" />
+                  <div className="absolute inset-0 bg-gradient-to-t from-black via-black/40 to-transparent z-10 md:hidden" />
                  
                   {/* HERO CONTENT */}
                   <div className="absolute inset-0 z-20 flex flex-col justify-end pb-8 px-6 md:pb-32 md:px-20 text-center md:text-left items-center md:items-start">
